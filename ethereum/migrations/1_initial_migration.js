8026760409ecaa578f832d232b3d707cbe05f792
lsunsi <sunsi.lucas@gmail.com>
2020-09-15 18:00:06 -0300

    WIP

--- ethereum/migrations/1_initial_migration.js
+++ ethereum/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Cryptobets = artifacts.require("Cryptobets");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Cryptobets);
 };
