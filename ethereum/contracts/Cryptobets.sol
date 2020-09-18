4fb05936d4cd5c5bdaf81f691fd2876a758f990c
lsunsi <sunsi.lucas@gmail.com>
2020-09-18 17:13:34 -0300

    wip

--- ethereum/contracts/Cryptobets.sol
+++ ethereum/contracts/Cryptobets.sol
@@ -2,134 +2,137 @@
 pragma solidity ^0.7.0;
 
 contract Cryptobets {
-    enum PoolState {
-        NotCreated,
-        Open,
-        InProgress,
-        Closed
+  enum PoolState { NotCreated, Open, InProgress, Closed }
+  enum BetOption { None, Bear, Bull }
+
+  struct Pool {
+    PoolState state;
+    int256 startPrice;
+    uint256 startTimestamp;
+    int256 endPrice;
+    uint256 endTimestamp;
+    uint256 upBalance;
+    uint256 downBalance;
+    BetOption winner;
+  }
+
+  struct Bet {
+    BetOption option;
+    uint256 value;
+  }
+
+  address owner;
+
+  uint256[] public poolIds;
+  mapping(uint256 => Pool) public pools;
+  mapping(uint256 => mapping(address => Bet)) public bets;
+  mapping(uint256 => mapping(address => bool)) public withdraws;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner, "Only owner");
+    _;
+  }
+
+  function poolIdsLength() public view returns (uint256) {
+    return poolIds.length;
+  }
+
+  function createPool(uint256 _startTimestamp, uint256 _endTimestamp)
+    public
+    onlyOwner
+    returns (uint256)
+  {
+    uint256 newPoolId = poolIds.length;
+    poolIds.push(newPoolId);
+
+    Pool memory newPool = Pool({
+      state: PoolState.Open,
+      startPrice: 0,
+      endPrice: 0,
+      startTimestamp: _startTimestamp,
+      endTimestamp: _endTimestamp,
+      upBalance: 0,
+      downBalance: 0,
+      winner: BetOption.None
+    });
+    pools[newPoolId] = newPool;
+
+    return newPoolId;
+  }
+
+  function startPool(uint256 _poolId, int256 _startPrice) public onlyOwner {
+    require(pools[_poolId].state == PoolState.Open, "Invalid pool state");
+
+    pools[_poolId].startPrice = _startPrice;
+    pools[_poolId].state = PoolState.InProgress;
+  }
+
+  function closePool(uint256 _poolId, int256 _endPrice) public onlyOwner {
+    require(pools[_poolId].state == PoolState.InProgress, "Invalid pool state");
+
+    pools[_poolId].endPrice = _endPrice;
+    pools[_poolId].state = PoolState.Closed;
+
+    if (pools[_poolId].endPrice > pools[_poolId].startPrice) {
+      pools[_poolId].winner = BetOption.Bull;
+    } else if (pools[_poolId].endPrice < pools[_poolId].startPrice) {
+      pools[_poolId].winner = BetOption.Bear;
+    }
+  }
+
+  function placeBet(uint256 _poolId, BetOption _option) public payable {
+    require(msg.value >= 1 ether, "Minimum bet value");
+    require(pools[_poolId].state == PoolState.Open, "Invalid pool state");
+    require(
+      _option == BetOption.Bull || _option == BetOption.Bear,
+      "Invalid bet option"
+    );
+
+    // Should be extended to accept increasing bets
+    require(
+      bets[_poolId][msg.sender].option == BetOption.None,
+      "Bet already placed"
+    );
+
+    bets[_poolId][msg.sender] = Bet({ option: _option, value: msg.value });
+
+    if (_option == BetOption.Bull) {
+      pools[_poolId].upBalance += msg.value;
+    } else if (_option == BetOption.Bear) {
+      pools[_poolId].downBalance += msg.value;
+    }
+  }
+
+  function withdraw(uint256 _poolId) public payable {
+    require(pools[_poolId].state == PoolState.Closed, "Pool not closed");
+
+    Bet memory bet = bets[_poolId][msg.sender];
+    require(bet.value >= 1 ether, "Did not bet");
+
+    require(bet.option == pools[_poolId].winner, "Nothing to redeem");
+    require(!withdraws[_poolId][msg.sender], "Already got the reward");
+
+    withdraws[_poolId][msg.sender] = true;
+
+    Pool memory pool = pools[_poolId];
+
+    uint256 winnerBalance;
+    if (pool.winner == BetOption.Bull) {
+      winnerBalance = pool.upBalance;
+    } else if (pool.winner == BetOption.Bear) {
+      winnerBalance = pool.downBalance;
+    } else {
+      assert(false);
     }
 
-    enum BetOption {None, Bear, Bull}
+    uint256 withdrawValue = ((pool.upBalance + pool.downBalance) *
+      bet.value *
+      99) / (winnerBalance * 100);
 
-    struct Pool {
-        PoolState state;
-        int256 startPrice;
-        uint256 startTimestamp;
-        int256 endPrice;
-        uint256 endTimestamp;
-        uint256 upBalance;
-        uint256 downBalance;
-        BetOption winner;
-    }
-
-    struct Bet {
-        BetOption option;
-        uint256 value;
-    }
-
-    address owner;
-
-    uint256[] public poolIds;
-    mapping(uint256 => Pool) public pools;
-    mapping(uint256 => mapping(address => Bet)) public bets;
-    mapping(uint256 => mapping(address => bool)) public withdraws;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only owner");
-        _;
-    }
-
-    function poolIdsLength() public view returns (uint256) {
-        return poolIds.length;
-    }
-
-    function createPool(uint256 _startTimestamp, uint256 _endTimestamp) public onlyOwner returns (uint256) {
-        uint256 newPoolId = poolIds.length;
-        poolIds.push(newPoolId);
-
-        Pool memory newPool = Pool({
-            state: PoolState.Open,
-            startPrice: 0,
-            endPrice: 0,
-            startTimestamp: _startTimestamp,
-            endTimestamp: _endTimestamp,
-            upBalance: 0,
-            downBalance: 0,
-            winner: BetOption.None
-        });
-        pools[newPoolId] = newPool;
-
-        return newPoolId;
-    }
-
-    function startPool(uint256 _poolId, int256 _startPrice) public onlyOwner {
-        require(pools[_poolId].state == PoolState.Open, "Invalid pool state");
-
-        pools[_poolId].startPrice = _startPrice;
-        pools[_poolId].state = PoolState.InProgress;
-    }
-
-    function closePool(uint256 _poolId, int256 _endPrice) public onlyOwner {
-        require(pools[_poolId].state == PoolState.InProgress, "Invalid pool state");
-
-        pools[_poolId].endPrice = _endPrice;
-        pools[_poolId].state = PoolState.Closed;
-
-        if (pools[_poolId].endPrice > pools[_poolId].startPrice) {
-            pools[_poolId].winner = BetOption.Bull;
-        } else if (pools[_poolId].endPrice < pools[_poolId].startPrice) {
-            pools[_poolId].winner = BetOption.Bear;
-        }
-    }
-
-    function placeBet(uint256 _poolId, BetOption _option) public payable {
-        require(msg.value >= 1 ether, "Minimum bet value");
-        require(pools[_poolId].state == PoolState.Open, "Invalid pool state");
-        require(_option == BetOption.Bull || _option == BetOption.Bear, "Invalid bet option");
-
-        // Should be extended to accept increasing bets
-        require(bets[_poolId][msg.sender].option == BetOption.None, "Bet already placed");
-
-        bets[_poolId][msg.sender] = Bet({
-            option: _option,
-            value: msg.value
-        });
-
-        if (_option == BetOption.Bull) {
-            pools[_poolId].upBalance += msg.value;
-        } else if (_option == BetOption.Bear) {
-            pools[_poolId].downBalance += msg.value;
-        }
-    }
-
-    function withdraw(uint _poolId) public payable {
-        require(pools[_poolId].state == PoolState.Closed, "Pool not closed");
-
-        Bet memory bet = bets[_poolId][msg.sender];
-        require(bet.value >= 1 ether, "Did not bet");
-
-        require(bet.option == pools[_poolId].winner, "Nothing to redeem");
-        require(!withdraws[_poolId][msg.sender], "Already got the reward");
-
-        withdraws[_poolId][msg.sender] = true;
-
-        Pool memory pool = pools[_poolId];
-
-        uint256 winnerBalance;
-        if (pool.winner == BetOption.Bull) {
-            winnerBalance = pool.upBalance;
-        } else if (pool.winner == BetOption.Bear) {
-            winnerBalance = pool.downBalance;
-        } else {
-            assert(false);
-        }
-
-        uint256 withdrawValue = ((pool.upBalance + pool.downBalance) * bet.value * 99) / (winnerBalance * 100);
-
-        msg.sender.transfer(withdrawValue);
-    }
+    msg.sender.transfer(withdrawValue);
+  }
 }
--- ethereum/test/aaa.test.js
+++ ethereum/test/aaa.test.js
@@ -1,24 +1,71 @@
-const { expect, use } = require('chai');
-const { BigNumber, constants: { WeiPerEther } } = require('ethers');
-const { deployContract, MockProvider, solidity } = require('ethereum-waffle');
-const Cryptobets = require('../build/contracts/Cryptobets.json');
+const { utils } = require("ethers");
+const { deployContract, MockProvider, solidity } = require("ethereum-waffle");
+const Cryptobets = require("../build/contracts/Cryptobets.json");
 
-use(solidity);
+test("does it fucking work? wtf", async () => {
+  const [
+    ownerWallet,
+    wallet1,
+    wallet2,
+    wallet3,
+  ] = new MockProvider().getWallets();
 
-test('does it fucking work? wtf', async () => {
-    const [ownerWallet, wallet1, wallet2] = new MockProvider().getWallets();
+  const contract = await deployContract(ownerWallet, Cryptobets);
 
-    const contract = await deployContract(ownerWallet, Cryptobets);
+  const contractWallet1 = contract.connect(wallet1);
+  const contractWallet2 = contract.connect(wallet2);
+  const contractWallet3 = contract.connect(wallet3);
 
-    const poolId = (await contract.createPool(1, 2)).value;
+  // Pool creation by the owner
+  const poolId = await contract.createPool(1, 2).then((r) => r.value);
+  await contract.pools(poolId).then((pool) => {
+    expect(pool.startTimestamp.toString()).toBe("1");
+    expect(pool.endTimestamp.toString()).toBe("2");
+    expect(pool.state).toBe(1);
+  });
 
-    const contractWallet1 = contract.connect(wallet1);
-    const contractWallet2 = contract.connect(wallet2);
+  // The bets are placed in the pool by the users
+  await contractWallet1.placeBet(poolId, 1, {
+    value: utils.parseEther("2"),
+    gasLimit: 100000,
+  });
+  await contractWallet2.placeBet(poolId, 1, {
+    value: utils.parseEther("3"),
+    gasLimit: 100000,
+  });
+  await contractWallet3.placeBet(poolId, 2, {
+    value: utils.parseEther("4"),
+    gasLimit: 100000,
+  });
 
+  // The pool is started and closed by the owner favoring the bears
+  await contract.startPool(poolId, 300);
+  await contract.pools(poolId).then((pool) => {
+    expect(pool.startPrice.toString()).toBe("300");
+    expect(pool.winner).toBe(0);
+    expect(pool.state).toBe(2);
+  });
 
-    console.log(await contractWallet1.placeBet(poolId, 1, { value: WeiPerEther, gasLimit: WeiPerEther }));
+  await contract.closePool(poolId, 200);
+  await contract.pools(poolId).then((pool) => {
+    expect(pool.endPrice.toString()).toBe("200");
+    expect(pool.winner).toBe(1);
+    expect(pool.state).toBe(3);
+  });
 
+  // Winners withdrawal their prizes
+  const wallet1BalanceBefore = await wallet1.getBalance();
+  const wallet2BalanceBefore = await wallet2.getBalance();
 
-    // expect((await contract.poolIdsLength()).toString()).equal('1');
+  await contractWallet1.withdraw(poolId);
+  await contractWallet2.withdraw(poolId);
+
+  const wallet1BalanceAfter = await wallet1.getBalance();
+  const wallet2BalanceAfter = await wallet2.getBalance();
+
+  const wallet1BalanceDelta = wallet1BalanceAfter.sub(wallet1BalanceBefore);
+  expect(wallet1BalanceDelta).toEqual(utils.parseEther("3.564"));
+
+  const wallet2BalanceDelta = wallet2BalanceAfter - wallet2BalanceBefore;
+  expect(wallet2BalanceDelta).toBe(utils.parseEther("5.346"));
 });
-
